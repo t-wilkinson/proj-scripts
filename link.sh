176b11d3d7784d1e11c2fa4aa775048235c96324
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-04-06 22:46:05 -0400

    restructured dotfiles -> ~/.config; updated link.sh to match this;

--- link.sh
+++ link.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 # Links all files in ~/dotfiles folder to their respective position
 
-DOT=$HOME/dev/dotfiles
+DOT=$HOME/.config
 ZSH=${ZDOTDIR:-$HOME}
 
-mkdir -p $ZSH
 mkdir -p $HOME/.xmonad/
-mkdir -p $HOME/.vim/colors/
-mkdir -p $HOME/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/
+ln -sf $DOT/xmonad/xmonad.hs $HOME/.xmonad
 
-ln -sf $DOT/mono.vim $HOME/.vim/colors/
-ln -sf $DOT/darcula.vim $HOME/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/
-ln -sf $DOT/xmonad.hs $HOME/.xmonad
-
-home=(.aliases .p10k.zsh .tmux.conf .xinitrc .Xresources .XCompose .vimrc)
+home=(aliases p10k.zsh tmux.conf xinitrc Xresources XCompose vimrc)
 for f in ${home[@]}; do
-    ln -sf $DOT/$f $HOME
+    ln -sf $DOT/dotfiles/$f $HOME/.$f
 done
 
+mkdir -p $ZSH
 zsh=(zlogin zlogout zpreztorc zprofile zshenv zshrc)
 for f in ${zsh[@]}; do
-    ln -sf $DOT/.$f $ZSH/
-    ln -sf $DOT/.$f $ZSH/.zprezto/runcoms/$f
+    ln -sf $DOT/zsh/.$f $ZSH/
+    ln -sf $DOT/zsh/.$f $ZSH/.zprezto/runcoms/$f
 done
